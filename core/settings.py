2c0d890b42fe9084f40388b63f9dd86000164082
Davimteixeira <daviteixeira077@gmail.com>
2023-05-19 15:22:51 -0300

    dando o primeiro migrate

--- core/settings.py
+++ core/settings.py
@@ -78,10 +78,14 @@ WSGI_APPLICATION = 'core.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME':'recipes',
+        'USER':'user_con',
+        'PASSWORD':'12345678',
+        'HOST':'localhost',
     }
 }
+
 
 
 # Password validation
