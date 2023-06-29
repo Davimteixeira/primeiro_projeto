01bd15ff8d1edd52923fb3ac0b86d3f7c8702116
Davimteixeira <daviteixeira077@gmail.com>
2023-06-29 14:33:23 -0300

    Adicionando o campo para o slug ser unico nas receitas

--- recipes/tests.py
+++ recipes/tests.py
@@ -1,5 +1,11 @@
 from django.test import TestCase
 
 class RecipeURlsTest(TestCase):
-    def test_the_pytest_is_ok(self):
-        ...
+    
+    def test_recipe_home_url_is_correct(self):
+        assert 1 != 1
+        # home_url = reverse('recipes:home')
+        # self.assertEqual(home_url, '/')
+    
+    
+
