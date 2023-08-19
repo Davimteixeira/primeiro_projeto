82a641ee5d5eed295a30103077939a2f4f5c353a
Davimteixeira <daviteixeira077@gmail.com>
2023-08-19 15:30:31 -0300

    refatorando pagination

--- recipes/views.py
+++ recipes/views.py
@@ -1,34 +1,39 @@
 from django.db.models import Q
 from django.http.response import Http404
 from django.shortcuts import render, get_list_or_404, get_object_or_404
-from django.http import HttpResponse
 from .models import Recipe
 from django.core.paginator import Paginator
+from utils.pagination import make_pagination
 
 def home(request):
     recipes = Recipe.objects.filter(
         is_published=True
     ).order_by('-id')
     
-    current_page = request.GET.get('page',1)
-    paginator = Paginator(recipes, 9)
-    page_obj = paginator.get_page(current_page)
+    page_obj, pagination_range = make_pagination(request, recipes, 9)
     
     return render(request, 'recipes/pages/home.html', context={
         'recipes': page_obj,
+        'pagination_range': pagination_range
     })
 
 
-def category(request, category_id):
+def category(request, category_id): 
+    
+    
     recipes = get_list_or_404(
         Recipe.objects.filter(
+            
             category__id=category_id,
             is_published=True,
         ).order_by('-id')
     )
     
+    page_obj, pagination_range = make_pagination(request, recipes, 9)
+    
     return render(request, 'recipes/pages/category.html', context={
-        'recipes': recipes,
+        'recipes': page_obj,
+        'pagination_range' : pagination_range, 
         'title': f'{recipes[0].category.name} - Category | '
     })
 
@@ -50,11 +55,12 @@ def search(request):
         is_published=True
     ).order_by('-id')
     
-    if not search_term:
-        raise Http404()
+    page_obj, pagination_range = make_pagination(request, recipes, 9)
     
     return render(request, 'recipes/pages/search.html', {
         'page_title': f'Search for "{search_term}" |',
         'search_term': search_term,
-        'recipes': recipes,        
+        'recipes': recipes,  
+        'pagination_range': pagination_range,   
+        'additional_url_query': f'&q={search_term}',   
     })
--- utils/pagination.py
+++ utils/pagination.py
@@ -2,12 +2,6 @@ import math
 
 from django.core.paginator import Paginator
 
-# python -c
-# "import string as s;from random import SystemRandom as
-# sr;print(''.join(sr().choices(s.ascii_letters +
-# s.punctuation, k=64)))"
-
-
 def make_pagination_range(
     page_range,
     qty_pages,
@@ -41,19 +35,19 @@ def make_pagination_range(
     }
 
 
-# def make_pagination(request, queryset, per_page, qty_pages=4):
-#     try:
-#         current_page = int(request.GET.get('page', 1))
-#     except ValueError:
-#         current_page = 1
+def make_pagination(request, queryset, per_page, qty_pages=4):
+    try:
+         current_page = int(request.GET.get('page', 1))
+    except ValueError:
+         current_page = 1
 
-#     paginator = Paginator(queryset, per_page)
-#     page_obj = paginator.get_page(current_page)
+    paginator = Paginator(queryset, per_page)
+    page_obj = paginator.get_page(current_page)
 
-#     pagination_range = make_pagination_range(
-#         paginator.page_range,
-#         qty_pages,
-#         current_page
-#     )
+    pagination_range = make_pagination_range(
+         paginator.page_range,
+         qty_pages,
+         current_page
+     )
 
-#     return page_obj, pagination_range
+    return page_obj, pagination_range
