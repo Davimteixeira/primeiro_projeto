7a51f80c07a138e9aac739eb5d6f0982c8edc990
Davi Magalhães <t_dmteixeira@sfiec.org.br>
2024-03-14 15:39:20 -0300

    change: atualizando minha view, e criando a classe delete, adicionando as urls

--- apps/authors/views/all.py
+++ apps/authors/views/all.py
@@ -6,7 +6,6 @@ from django.shortcuts import redirect, render
 from django.urls import reverse
 from apps.authors.forms import LoginForm, RegisterForm
 from apps.recipes.models import Recipe
-from apps.authors.forms.recipe_form import AuthorRecipeForm
 
 def register_view(request):
     register_form_data = request.session.get('register_form_data', None)
@@ -84,91 +83,3 @@ def dashboard(request):
                   context={
                       'recipes':recipes,
                  })
-
-@login_required(login_url='authors:login', redirect_field_name='next')
-def dashboard_recipe_edit(request, id):
-    recipe = Recipe.objects.filter(
-        is_published=False,
-        author=request.user,
-        pk=id,
-    ).first()
-
-    if not recipe:
-        raise Http404()
-    
-    form = AuthorRecipeForm(
-        data=request.POST or None,
-        files=request.FILES or None,
-        instance=recipe
-    )
-
-    if form.is_valid():
-        recipe = form.save(commit=False)
-
-        recipe.author = request.user
-        recipe.preparationa_steps_is_html = False
-        recipe.is_published = False
-
-        recipe.save()
-
-        messages.success(request, 'sua receita foi salva por sucesso!')
-        return redirect(reverse('authors:dashboard_recipe_edit', args=(id,)))
-
-    return render(
-        request,
-        'authors/pages/dashboard_recipe.html',
-        context={
-            'form':form
-        }
-    )
-
-@login_required(login_url='authors:login', redirect_field_name='next')
-def dashboard_recipe_new(request):
-    form = AuthorRecipeForm(
-        data=request.POST or None,
-        files=request.FILES or None,
-    )
-
-    if form.is_valid():
-        recipe: Recipe = form.save(commit=False)
-
-        recipe.author = request.user
-        recipe.preparation_steps_is_html = False
-        recipe.is_published = False
-
-        recipe.save()
-
-        messages.success(request, 'Salvo com sucesso!')
-        return redirect(
-            reverse('authors:dashboard_recipe_edit', args=(recipe.id,))
-        )
-
-    return render(
-        request,
-        'authors/pages/dashboard_recipe.html',
-        context={
-            'form': form,
-            'form_action': reverse('authors:dashboard_recipe_new')
-        }
-    )
-
-@login_required(login_url='authors:login', redirect_field_name='next')
-def dashboard_recipe_delete(request):
-    if not request.POST:
-        raise Http404()
-
-    POST = request.POST
-    id = POST.get('id')
-    
-    recipe = Recipe.objects.filter(
-        is_published=False,
-        author=request.user,
-        pk=id,
-    ).first()
-
-    if not recipe:
-        raise Http404()
-
-    recipe.delete()
-    messages.success(request, 'Deleted successfully.')
-    return redirect(reverse('authors:dashboard'))
--- apps/authors/views/dashboard_recipe.py
+++ apps/authors/views/dashboard_recipe.py
@@ -5,13 +5,28 @@ from django.shortcuts import redirect, render
 from django.urls import reverse
 from django.views import View
 from apps.recipes.models import Recipe
+from django.utils.decorators import method_decorator
+from django.contrib.auth.decorators import login_required
+
+@method_decorator(
+    login_required(login_url='authors:login', redirect_field_name='next'),
+    name='dispatch'
+)
+class DashboardRecipe(View):    
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+    def setup(self, *args, **kwargs):
+        return super().setup(*args, **kwargs)
+
+    def dispatch(self, *args, **kwargs):
+        return super().dispatch(*args, **kwargs)
 
 
-class DashboardRecipe(View):
-    def get_recipe(self, id):
+    def get_recipe(self, id=None):
         recipe = None
 
-        if id:
+        if id is not None:
             recipe = Recipe.objects.filter(
                 is_published=False,
                 author=self.request.user,
@@ -32,12 +47,12 @@ class DashboardRecipe(View):
             }
         )
     
-    def get(self, request, id):
+    def get(self, request, id=None):
         recipe = self.get_recipe(id)
         form = AuthorRecipeForm(instance=recipe)
         return self.render_recipe(form)
     
-    def post(self, request, id):
+    def post(self, request, id=None):
         recipe = self.get_recipe(id)
         form = AuthorRecipeForm(
             data=request.POST or None,
@@ -56,7 +71,22 @@ class DashboardRecipe(View):
 
             messages.success(request, 'sua receita foi salva por sucesso!')
             return redirect(
-                reverse('authors:dashboard_recipe_edit', args=(id,))
+                reverse('authors:dashboard_recipe_edit', args=(
+                    recipe.id,
+                    )
+                )
             )
 
         return self.render_recipe(form)
+
+
+@method_decorator(
+    login_required(login_url='authors:login', redirect_field_name='next'),
+    name='dispatch'
+)
+class DashboardRecipeDelete(DashboardRecipe):
+    def post(self, *args, **kwargs):
+        recipe = self.get_recipe(self.request.POST.get('id'))
+        recipe.delete()
+        messages.success(self.request, 'Deleted successfully.')
+        return redirect(reverse('authors:dashboard'))
