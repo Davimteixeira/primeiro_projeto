874cbddc93a17e9eaa96b6a1a137c708c9f57352
Davi Magalhães <t_dmteixeira@sfiec.org.br>
2024-04-07 14:33:29 -0300

    criando view e rota pra api

--- apps/recipes/views.py
+++ apps/recipes/views.py
@@ -2,6 +2,7 @@ from typing import List
 from django.db.models import Q
 from django.http.response import Http404, HttpResponse as HttpResponse
 from django.shortcuts import get_object_or_404, render
+from django.forms.models import model_to_dict
 from django.http import JsonResponse
 from .models import Recipe
 from django.core.paginator import Paginator
@@ -126,4 +127,24 @@ class RecipeDetail(DetailView):
 
         return ctx
 
+class RecipeDetailAPI(RecipeDetail):
+    def render_to_response(self, context, **response_kwargs):
+        recipe = self.get_context_data()['recipe']
+        recipe_dict = model_to_dict(recipe)
 
+        recipe_dict['created_at'] = str(recipe.created_at)
+        recipe_dict['updated_at'] = str(recipe.updated_at)
+
+        if recipe_dict.get('cover'):
+            recipe_dict['cover'] = self.request.build_absolute_uri() + \
+                recipe_dict['cover'].url[1:]
+        else:
+            recipe_dict['cover'] = ''
+
+        del recipe_dict['is_published']
+        del recipe_dict['preparation_steps_is_html']
+
+        return JsonResponse(
+            recipe_dict,
+            safe=False,
+        )
